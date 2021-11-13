cf2e767662d4da83545435084f6855d64e6e84b8
Niina Kettunen <niina.kettunen@edu.sasky.fi>
2021-11-13 16:30:09 +0200

    hakemistoviittaukset vakioiksi

--- config/config.php
+++ config/config.php
@@ -10,4 +10,17 @@ $config = array(
     )
 );
 
+// Tässä määritellään joukko vakioita, joissa määritellään hakemistopolut sovelluksen eri palikoille
+// Sovelluksessa joudutaan viittaamaan useammassa skriptissä projektin eri hakemistoihin, 
+// kuten tietokantaa käsitteleviin malleihin tai sivurunkoihin, joista sivut muodostetaan. 
+// Jatkossa ylläpidettävyyden kannalta on helpompaa määritellä projektin kansioille vakiot
+// joita käytetään jatkossa näihin kansioihin viitattaessa
+
+define("PROJECT_ROOT", dirname(__DIR__) . "/");
+define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
+define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
+define("MODEL_DIR", PROJECT_ROOT . "src/model/");
+define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
+
+
 ?>
