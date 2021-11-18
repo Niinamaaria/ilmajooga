67e2433aee582157264c3c8bc9ccefab21bff430
Niina Kettunen <niina.kettunen@edu.sasky.fi>
2021-11-18 19:24:12 +0200

    lomakkeen ja kuvien tyylimääritykset

--- src/view/template.php
+++ src/view/template.php
@@ -16,8 +16,12 @@
     <footer>
         <hr>
         <div>Ilmajoogatapahtuma Ilma by Joogastudio Nisaya</div>
-        <image src="../ilmajooga/public/images/yoga5.jpg" alt="Yoga">
-            <p>image by: Oksana Taran
+        <image src="../ilmajooga/public/images/yoga1.jpg" alt="Yoga">
+        <image src="../ilmajooga/public/images/yoga2.jpg" alt="Yoga">
+        <image src="../ilmajooga/public/images/yoga3.jpg" alt="Yoga">
+            <p>Photo by <a href="https://unsplash.com/@wesleyphotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Wesley Tingey</a> on <a href="https://unsplash.com/s/photos/yoga?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            <p>Photo by <a href="https://unsplash.com/@jareddrice?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jared Rice</a> on <a href="https://unsplash.com/s/photos/yoga?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            <p>Photo by <a href="https://unsplash.com/@kikekiks?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">kike vega</a> on <a href="https://unsplash.com/s/photos/yoga?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
     </footer>
 </body>
 </html>
