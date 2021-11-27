c1d75e12c6baac757c740e6fb9e2eef3a95a8ee5
Niina Kettunen <niina.kettunen@edu.sasky.fi>
2021-11-27 16:00:29 +0200

    ilmoittaudu-nappi kurssin sivulle

--- src/view/kurssi.php
+++ src/view/kurssi.php
@@ -9,3 +9,19 @@
 <div><?=$kurssi['kuvaus']?></div>
 <div>Alkaa: <?=$start->format('j.n.Y G:i')?></div>
 <div>Loppuu: <?=$end->format('j.n.Y G:i')?></div>
+
+<?php
+if ($loggeduser) {
+    if (!$ilmoittautuminen) {
+      echo "<div class='flexarea'><a href='ilmoittaudu?id=$kurssi[idkurssi]' class='button'>ILMOITTAUDU</a></div>";    
+    } else {
+      echo "<div class='flexarea'>";
+      echo "<div>Olet ilmoittautunut tapahtumaan!</div>";
+      echo "<a href='peru?id=$kurssi[idkurssi]' class='button'>PERU ILMOITTAUTUMINEN</a>";
+      echo "</div>";
+    }
+  }
+?>
+
+
+
